021bb0d63d3a84b5fdbeb3f60d2c3f53ba2f65d5
ahmad <ahmad.ben.hamad2004@gmail.com>
2024-06-03 23:52:42 +0100

    login/logout

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,21 +1,49 @@
 <script setup>
-
-const emit = defineEmits(['test']);
-function handelsubmit  ()  {
-    window.location.hash = "#/"
-    emit('test')
-}
 let username = ''
 let password = ''
+
+const emit = defineEmits(['test']);
+function handelsubmit() {
+    fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        'username': username,
+        'password': password
+    })
+})
+.then(response => {
+    // Check if the request was successful
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    // Parse the response as JSON
+    return response.json();
+})
+.then(response => {
+    if (response.state) {
+        // Assuming emit is a valid function in your context
+        emit('test');
+        window.location.hash = "#/";
+    } else {
+        console.log("user not found");
+    }
+})
+.catch(error => {
+    console.error('Error:', error);
+});
+}
 </script>
 
 <template>
     <div id="container">
-            <label for="username">Username: </label>
-            <input type="text" v-model="username">
-            <label for="password">Password: </label>
-            <input type="password" v-model="password">
-            <button type="submit" @click="handelsubmit">Login</button>
+        <label for="username">Username: </label>
+        <input type="text" v-model="username">
+        <label for="password">Password: </label>
+        <input type="password" v-model="password">
+        <button type="submit" @click="handelsubmit">Login</button>
     </div>
 </template>
 
