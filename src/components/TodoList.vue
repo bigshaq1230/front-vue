7dcebcd830d14fb9af517b5d3233ed7e3fffcd8e
ahmad <ahmad.ben.hamad2004@gmail.com>
2024-06-08 03:29:13 +0100

    a bit better and actaully working

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -6,16 +6,18 @@ let logged_in = ref(props.state)
 let username = ref(props.username)
 /*
  flags = enum {
-    'local' means the item is stored on the user pc
+    'added' newly added so stored locally
     'removed' to be removed from db
     'db' means it's stored on the db
     'edited' really thinking about implimenting this later maybe even now actually
     'skip' doesn't matter
 }
+    🤓🤓🤓🤓🤓🤓
 */
+
 let list = ref(JSON.parse(localStorage.getItem('list')) || [
-    { label: "do something", state: true ,flag:'local'},
-    { label: "learn backend", state: true ,flag:'local'}
+    { label: "do something", state: true, flag: 'local' },
+    { label: "learn backend", state: true, flag: 'local' }
 ])
 let input = ref("")
 
@@ -28,17 +30,28 @@ function update() {
 if (logged_in.value) {  // this should work
     pull()
 }
+
 function push() {
+    let new_list = []
+    for (let i = 0; i < list.value.length; i++) {
+        const element = list.value[i];
+        if (element.flag !== 'db') {
+            new_list.push(element)
+        }
+        if (element.flag === 'removed') {
+            list.value.splice(i, 1)
+        }
+    }
     fetch('http://localhost:3000/push', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        'list': list.value,
-        'username': username.value
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'list': new_list,
+            'username': username.value
+        })
     })
-})
 
         .then(response => {
             // Check if the request was successful
@@ -51,6 +64,10 @@ function push() {
         .catch(error => {
             console.error('Error:', error);
         });
+    list.value.forEach(element => {
+        element.flag = 'db'
+    });
+
 }
 function pull() {
     console.log(username.value)
@@ -72,13 +89,23 @@ function pull() {
             return response.json();
         })
         .then(data => {
-            // god please help me
-            // gonna stop for now tomorrow will complete hopefully :)
-            let newList = []
+
             data.list.forEach(element => {
-                if
+                let index = list.value.findIndex((l) => l.label === element.label)
+                console.log(index)
+                if (index === -1) {
+                    list.value.push({
+                        label: element.label,
+                        state: element.state,
+                        flag: 'db'
+                    })
+                }
+                else {
+                    list.value[index].state = element.state
+                    list.value[index].flag = 'db'
+                }
+
             });
-            list.value = list.value + data.list;
         })
 }
 function onUserInactivity() {
@@ -106,19 +133,22 @@ function add() {
     list.value.push(
         {
             label: input.value,
-            state: true
+            state: true,
+            flag: 'added'
         }
     );
     input.value = "";
     update();
 }
 function remove(index) {
-    list.value.splice(index, 1);
+    list.value[index].flag = 'removed'
+    //list.value.splice(index, 1);
     update();
 
 }
 function done(index) {
     list.value[index].state = !list.value[index].state;
+    list.value[index].flag = 'edited'
     update();
 
 }
@@ -129,16 +159,16 @@ function done(index) {
         <input type="text" v-model="input">
         <button @click="add">Add</button>
         <ul>
-            <li v-for="(element, index) in list" :key="index">
+            <span  v-for="(element, index) in list" :key="index" >
+            <li  v-if="element.flag != 'removed'">
+                    <input type="checkbox" v-on:change="done(index)">
 
-                <input type="checkbox" v-on:change="done(index)">
+                    <span v-if="element.state">{{ element.label }}</span>
+                    <span v-else style="text-decoration: line-through;">{{ element.label }}</span>
 
-                <span v-if="element.state">{{ element.label }}</span>
-                <span v-else style="text-decoration: line-through;">{{ element.label }}</span>
-
-                <span @click="remove(index)" style="cursor: pointer;">❌</span>
-
-            </li>
+                    <span @click="remove(index)" style="cursor: pointer;">❌</span>
+                </li>
+            </span>
         </ul>
         <a v-if="logged_in === false" href="#/login"><button>Log In</button></a>
         <a v-if="logged_in === true" v-on:click="logged_in = false"> <button>Log Out</button></a>
